e79a863ed99a48614a873ce0367d18365f2fa570
MariusBongarts <marius.bongarts@external.sinnerschrader.com>
2023-01-30 08:35:42 +0100

    Change React setup in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { setupChromeMocks } from "./services/chrome-api-mock";
 
+const rootElement = document.createElement("div");
+rootElement.id = "chrome-history-dashboard";
+document.body.appendChild(rootElement);
+
 setupChromeMocks();
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <App />
